9749f09c0b4dc72279a17a47645001bfa96016bd
duncan.laidlaw <duncan.laidlaw@deeperthanblue.co.uk>
2019-09-21 19:32:42 +0100

    book details added

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -8,21 +8,13 @@ const BookContextProvider = props => {
     { title: 'name of the wind', author: 'patrick rothfuss', id: 1 },
     { title: 'the final empire', author: 'brandon sanderson', id: 2 }
   ]);
-
   const addBook = (title, author) => {
-    setBooks([
-      ...books,
-      {
-        title: title,
-        author: author,
-        id: uuid()
-      }
-    ]);
+    setBooks([...books, { title, author, id: uuid() }]);
   };
-
   const removeBook = id => {
     setBooks(books.filter(book => book.id !== id));
   };
+
   return (
     <BookContext.Provider value={{ books, addBook, removeBook }}>
       {props.children}
